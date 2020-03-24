e0e63ec4d41da3d0267f195e43727ba91de4e1a8
Jeet Dholakia <jeet.dholakia@peaas.co>
2020-03-24 15:26:00 +0530

    Added First Missing Problem
    Added First Missing Problem

--- Array/41-First Missing Positive.playground/Contents.swift
+++ Array/41-First Missing Positive.playground/Contents.swift
@@ -1,5 +1,69 @@
+/**
+* Primary idea: Use a set to hold number in the array and iterate through 1...nums.count to find the missing one
+*/
+
 class Solution {
+    /**
+     * Time Complexity: O(n^2)
+     */
     func firstMissingPositive(_ nums: [Int]) -> Int {
-        return 1
+        
+        if nums.count == 0 {
+            return 1
+        }
+        
+        var dict: [Int: Int] = [:]
+        
+        for num in nums {
+            dict[num] = 1
+        }
+        
+        for i in 1...nums.count {
+            if let _ = dict[i] {
+                continue
+            } else {
+                return i
+            }
+        }
+        
+        return nums.count + 1
+    }
+    
+    
+    /**
+     * Time complexity: O(n^2)
+     */
+    func firstMissingPositiveUsingArrayOnly(_ nums: [Int]) -> Int {
+        
+        if nums.count == 0 {
+            return 1
+        }
+        
+        for i in 1...nums.count {
+            if let _ = nums.firstIndex(of: i) {
+                continue
+            } else {
+                return i
+            }
+        }
+        
+        return nums.count + 1
+    }
+    
+    /**
+     * Time complexity: O(n)
+     */
+    func firstMissingPositiveUsingSet(_ nums: [Int]) -> Int {
+        let set = Set(nums)
+        
+        for i in 0..<nums.count {
+            if !set.contains(i + 1) {
+                return i + 1
+            }
+        }
+        
+        return nums.count + 1
     }
 }
+
+Solution().firstMissingPositiveUsingArrayOnly([-2])
